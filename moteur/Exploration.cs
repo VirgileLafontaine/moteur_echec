d9f49767eb6eb3351f93c7e52fea81c6dc576f37
Virgile Lafontaine <virgile.lafontaine@laposte.net>
2017-11-19 18:59:30 -0500

    evaluation simple

--- moteur/Exploration.cs
+++ moteur/Exploration.cs
@@ -8,6 +8,43 @@ namespace Moteur
 {
     public class Exploration
     {
+        //poids d'importance des pieces sur le terrain
+        public int poidsPion = 100, poidsCavalier = 320, poidsFou = 330, poidsTour = 500, poidsReine = 900, poidsRoi = 20000;
+
+        const UInt64 k1 = 0x5555555555555555; /*  -1/3   */
+        const UInt64 k2 = 0x3333333333333333; /*  -1/5   */
+        const UInt64 k4 = 0x0f0f0f0f0f0f0f0f; /*  -1/17  */
+        const UInt64 kf = 0x0101010101010101; /*  -1/255 */
+
+        public int count (UInt64 x) {
+                x = x - ((x >> 1) & k1); /* put count of each 2 bits into those 2 bits */
+                x = (x & k2) + ((x >> 2) & k2); /* put count of each 4 bits into those 4 bits */
+                x = (x + (x >> 4)) & k4; /* put count of each 8 bits into those 8 bits */
+                x = (x * kf) >> 56; /* returns 8 most significant bits of x + (x<<8) + (x<<16) + (x<<24) + ...  */
+                return (int)x;
+            }
+
+    //evaluation d'un environnement
+    public int evaluer(Environnement env)
+        {
+            Environnement.enumCouleurJoueur adv;
+            Environnement.enumCouleurJoueur joueur = env.getJoueur();
+            if (joueur == Environnement.enumCouleurJoueur.blanc){
+                adv = Environnement.enumCouleurJoueur.noir;
+            }
+            else
+            {
+                adv = Environnement.enumCouleurJoueur.noir;
+            }
+            int score = poidsRoi * (count(env.getRoi(joueur) - env.getRoi(adv)))
+              + poidsReine * (count(env.getReine(joueur) - env.getReine(adv)))
+              + poidsTour * (count(env.getTour(joueur) - env.getTour(adv)))
+              + poidsCavalier * (count(env.getCavalier(joueur) - env.getCavalier(adv)))
+              + poidsFou * (count(env.getFou(joueur) - env.getFou(adv)))
+              + poidsPion * (count(env.getPion(joueur) - env.getPion(adv)));
+
+            return score;
+        }
 
         public int alphaBeta(Environnement env, int alpha, int beta, int profondeurRestante)
         {
@@ -15,7 +52,7 @@ namespace Moteur
             int score = 0;
             if (alpha > beta) {return -1;}
             int bestscore = -99999;
-            if (profondeurRestante == 0) return rechercheCalme(alpha, beta);
+            if (profondeurRestante == 0) return rechercheCalme(alpha, beta, env);
             //TO DO : générer les environnements avec le dernier mouvement et historique mis a jour
             foreach (Environnement mouvement in listeMouvements)
             {
@@ -31,10 +68,27 @@ namespace Moteur
             }
             return bestscore;
         }
-        public int rechercheCalme(int alpha, int beta)
+        public int rechercheCalme(int alpha, int beta, Environnement env)
         {
-            //TO DO : quiescence search
-            return 0;
+            int score = 0;//pas sur
+            int standPat = evaluer(env);
+            if (standPat >= beta)
+                return beta;
+            if (alpha < standPat)
+                alpha = standPat;
+               /*
+            until(every_capture_has_been_examined)  {
+                MakeCapture();
+                score = -rechercheCalme(-beta, -alpha, env);
+                TakeBackMove();
+                
+                if (score >= beta)
+                    return beta;
+                if (score > alpha)
+                    alpha = score;
+            }*/
+            return alpha;
+            }
         }
     }
 }
