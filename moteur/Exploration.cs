5cfeb9dcf6c4204051438824b1293467f35c43a4
Virgile Lafontaine <virgile.lafontaine@laposte.net>
2017-12-15 02:27:48 -0500

    verification echec et mat

--- moteur/Exploration.cs
+++ moteur/Exploration.cs
@@ -130,6 +130,7 @@ namespace Moteur
             }
             if (remainingDepth == 0) return RechercheCalme(alpha, beta, env);
             Queue QueueEnvironments = MovesCalculator.ProchainsEnvironnements(ref env, env.CurrentPlayer, false); // false : all moves, not only captures
+            if (QueueEnvironments.Count==0) {return env; }
             ArrayList ListEnvironmentsRnd = Randomize(QueueEnvironments);
             List<Environment> ListEnvironments = new List<Environment>();
             foreach (Environment e in ListEnvironmentsRnd)
