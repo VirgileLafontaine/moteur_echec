7b2f35908ebf9a9163266b1aea4b535eac8f108f
lparant <laurieanne.parant@gmail.com>
2017-12-15 01:18:05 -0500

    mobilité initialisée à 0 par défaut

--- moteur/Environnement.cs
+++ moteur/Environnement.cs
@@ -15,7 +15,7 @@ namespace Moteur
         public int[] Board;
         public string[] Mvt;
         public int Score;
-        public int Mobility;
+        public int Mobility = 0;
         public int Ordre;
         public Environment(int joueurActuel, int[] board, Environment previous,string[] mvt)
         {
