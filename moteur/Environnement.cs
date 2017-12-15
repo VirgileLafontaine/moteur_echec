5237ddc5c63e61d6cddd308da04f927ec1fa7f7b
Virgile Lafontaine <virgile.lafontaine@laposte.net>
2017-12-15 02:24:49 -0500

    Revert "mobilité initialisée à 0 par défaut"
    This reverts commit 7b2f35908ebf9a9163266b1aea4b535eac8f108f.

--- moteur/Environnement.cs
+++ moteur/Environnement.cs
@@ -15,7 +15,7 @@ namespace Moteur
         public int[] Board;
         public string[] Mvt;
         public int Score;
-        public int Mobility = 0;
+        public int Mobility;
         public int Ordre;
         public Environment(int joueurActuel, int[] board, Environment previous,string[] mvt)
         {
